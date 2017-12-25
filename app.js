e99a8916268f9bb2df548212b07ba2395f41db5e
Vicente <pouyio@gmail.com>
2017-12-25 23:06:08 +0100

    lint

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ app.set('view engine', 'pug')
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.render('index', { key: process.env.VAPID_PUBLIC_KEY}));
+app.get('/', (req, res) => res.render('index', { key: process.env.VAPID_PUBLIC_KEY }));
 
 app.post('/subscribe', async (req, res) => {
 
   notifications.subscribe(req.body.subscription);
   res.sendStatus(200);
-  
+
 });
 
 app.delete('/subscribe', async (req, res) => {
-  
+
   notifications.unsubscribe(req.body.subscription);
   res.sendStatus(200);
 
@@ -38,16 +38,16 @@ app.post('/all-albums', async (req, res) => {
 
   notifications.notificate((await scrap())[0]);
   res.sendStatus(200);
-  
+
 });
 
 
 app.get('/*', (r, s) => s.sendStatus(404));
 
- const options = {
-                key: fs.readFileSync('/etc/letsencrypt/live/comic/privkey.pem'),
-                cert: fs.readFileSync('/etc/letsencrypt/live/comic/fullchain.pem')
-        }
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/comic/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/comic/fullchain.pem')
+}
 const httpsServer = https.createServer(options || {}, app);
 httpsServer.listen(process.env.PORT, () => console.log(`Notifier app is runnin on port ${process.env.PORT}!`));
 // app.listen(process.env.PORT, () => console.log('Node app is running on port ' + process.env.PORT));

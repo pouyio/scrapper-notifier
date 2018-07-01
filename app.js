b0d93e833d72f5c673d642c7cd8d4e9ba0146824
Vicente <pouyio@gmail.com>
2018-07-01 13:51:39 +0200

    fix reverse mutating source

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ app.set('view engine', 'pug')
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/', async (req, res) => res.render('index', { key: process.env.VAPID_PUBLIC_KEY, data: db.get('last_entries').value().reverse(), source: process.env.SOURCE_URL }));
+app.get('/', (req, res) => {
+  res.render('index', {
+    key: process.env.VAPID_PUBLIC_KEY,
+    data: db.get('last_entries').slice().reverse().value(),
+    source: process.env.SOURCE_URL
+  });
+});
 
 app.post('/subscribe', (req, res) => {
 
